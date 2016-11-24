a242ec30527c22d8a597a0f8bf21a43dadcc30cb
grindfather <memphis@ip0af57858.int.uvm.edu>
2016-11-23 21:16:57 -0500

    bigram counts
    bigram counts

--- cipher project /decrypt.py
+++ cipher project /decrypt.py
@@ -13,8 +13,8 @@ myfile = "cipher_text.txt"
 
 
 
-## Define a function to find out the frequency of all letters
-def freq(bla):
+## Define step1 --> letter counts
+def gram1(bla):
     ## Two lists
     alpha = []
     count = [1]*(len(bla))
@@ -27,7 +27,12 @@ def freq(bla):
         elif ch in alpha and (ch.isalpha() or ch == "{"): ## I cheated
              num = alpha.index(ch)
              count[num] += 1
-
+             
+    ## Display results
+    print("Letter \t Freq")
+    for i in range(len(alpha)):
+        print(alpha[i],"\t",count[i])
+        
     ## return values
     return alpha, count
 
@@ -38,12 +43,21 @@ def freq(bla):
 
 
 
-## Define step 1 --> Find e and replace it
-def step1(mystring):
-    print("\nStep1 \nLetter 'E' is the most frequently used letter in Englsih and so '",alpha[count.index(max(count))],"'is 'E'")
-    print("\nWe are going to replace all '",alpha[count.index(max(count))],"' with 'e' as followed:\n")
-    mystring = mystring.replace(alpha[count.index(max(count))],"e")
-    print(mystring)
+## Define step2 --> Find e and replace it
+def step2(mystring):
+    print("\nStep1\nLetter 'e' is the most frequently used letter in Englsih and so '",alpha1[count1.index(max(count1))],"'is 'e'")
+    print("\nWe are going to replace all '",alpha1[count1.index(max(count1))],"' with 'e' as followed:\n")
+    mystring = mystring.replace(alpha1[count1.index(max(count1))],"e") ## Replace e
+    mystring_ = ""
+    for ch in mystring:
+        if ch == "e":
+            mystring_ += ch
+        elif ch.isalpha():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring, mystring_
 
 
 
@@ -51,8 +65,29 @@ def step1(mystring):
 
 
 
-#### Define step 2 --> 
-##def step2(mystring):
+## Define step3 --> Produce Bigram Table
+def gram2(mystring):
+    ## Two lists
+    alpha = []
+    count = [1]*(len(mystring)) ## Should fuck up here
+    print("\nStep2\nBigram counts chart:")
+    print("P.S. Letter 'e' is been replaced already here.\n")
+    ## Compute alpha and count
+    for i in range(len(mystring)-1):
+        ch = mystring[i]+mystring[i+1]
+        if ch not in alpha and ch.isalpha():
+            alpha.append(ch)
+        elif ch in alpha and ch.isalpha():
+            num = alpha.index(ch)
+            count[num] += 1
+
+    ## Display results
+    print("Bigrams \t Freq")
+    for i in range(len(alpha)):
+        print(alpha[i],"\t","\t",count[i])
+                   
+    ## return values
+    return alpha, count
 
 
 
@@ -66,19 +101,18 @@ mystring = msg.read()
 msg.close()
 
 ## display the encrypted message
+print("Here is the encrypted message:\n")
 print(mystring)
-print("\n"*10)
+print("\n"*5)
 
-## display the frequency of all letters
-alpha, count = freq(mystring)
-print("Letter \t Freq")
-for i in range(len(alpha)):
-    print(alpha[i],"\t",count[i])
+## Step 1 --> letter counts
+alpha1, count1 = gram1(mystring)
 
-## step 1
-step1(mystring)
+## Step 2 --> Find e and replace it
+mystring, mystring_ = step2(mystring)
 
-## step 2
+## Step 3 --> Bigram counts
+alpha2, count2 = gram2(mystring)
 
 
 ## THE END ##
