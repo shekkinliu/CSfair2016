d367121104cdf402e78180846796dc386af05b73
grindfather <memphis@ip0af5776b.int.uvm.edu>
2016-11-30 18:52:01 -0500

    changes
    cracked 8/9letters
    —> next step ==> S

--- cipher project /decrypt.py
+++ cipher project /decrypt.py
@@ -27,17 +27,153 @@ diction1, alpha1, count1 = decrypt_functions.gram1(mystring)
 mystring_1 = decrypt_functions.find_e(mystring, alpha1)
 
 ## Bigram counts
-diction2, alpha2, count2 = decrypt_functions.gram2(mystring)
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_1)
 
 ## Trigram counts
-diction3, alpha3, count3 = decrypt_functions.gram3(mystring)
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_1)
 
 ## 4-gram counts
-diction4, alpha4, count4 = decrypt_functions.gram4(mystring)
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_1)
 
 ## Step 2 --> Find t,h and replace it
 ## e
 ## t h
 mystring_2 = decrypt_functions.find_t_h(mystring_1, alpha3)
 
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_2)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_2)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_2)
+
+## Step 3 --> Find n and replace it
+## e
+## t h
+## n
+mystring_3 = decrypt_functions.find_n(mystring_2, alpha3)
+
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_3)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_3)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_3)
+
+## Step 4 --> Find a and replace it
+## e
+## t h
+## n
+## a
+mystring_4 = decrypt_functions.find_a(mystring_3, alpha4)
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_4)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_4)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_4)
+
+## Step 5 --> Find d and replace it
+## e
+## t h
+## n
+## a
+## d
+mystring_5 = decrypt_functions.find_d(mystring_4, alpha3)
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_5)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_5)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_5)
+
+## Step 6 --> Find r and replace it
+## e
+## t h
+## n
+## a
+## d
+## r
+mystring_6 = decrypt_functions.find_r(mystring_5, alpha2)
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_6)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_6)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_6)
+
+## Step 7 --> Find o and replace it
+## e
+## t h
+## n
+## a
+## d
+## r
+## o
+mystring_7 = decrypt_functions.find_o(mystring_6, alpha2)
+
+########################
+## NEXT STEP ##
+########################
+
+
+## Bigram counts
+diction2, alpha2, count2 = decrypt_functions.gram2(mystring_7)
+
+## Trigram counts
+diction3, alpha3, count3 = decrypt_functions.gram3(mystring_7)
+
+## 4-gram counts
+diction4, alpha4, count4 = decrypt_functions.gram4(mystring_7)
+
+## Step 7 --> Find i and replace it
+## e
+## t h
+## n
+## a
+## d
+## r
+## o
+## i
+mystring_8 = decrypt_functions.find_i(mystring_7, alpha2)
 ## THE END ##
--- cipher project /decrypt_functions.py
+++ cipher project /decrypt_functions.py
@@ -8,7 +8,7 @@
 import operator
 
 ## Constants
-print_num = 5
+print_num = 10
 
 ## Define gram1 --> letter counts
 def gram1(mystring):
@@ -155,9 +155,13 @@ def gram4(mystring):
     return diction, sorted_alpha, sorted_count
 
 
-#######################
-## END OF N-GRAM COUNTS 
-#######################
+###################################################################################################################
+#######################                     END OF N-GRAM COUNTS 
+###################################################################################################################
+
+###################################################################################################################
+#######################                     FIND E 
+###################################################################################################################
 
 ## Define step1 --> Find e and replace it
 def find_e(mystring, alpha):
@@ -166,44 +170,189 @@ def find_e(mystring, alpha):
     mystring = mystring.replace(alpha[0],"e") ## Replace e
     mystring_ = ""
     for ch in mystring:
-        if ch == "e":
-            mystring_ += ch
-        elif ch.isalpha():
+        if ch.isupper():
             mystring_ += "_"
         else:
             mystring_ += ch
     print(mystring_)
     return mystring
 
+###################################################################################################################
+#######################                     FIND T H 
+###################################################################################################################
 
 ## Define step2 --> Find t,h and replace it
 def find_t_h(mystring, alpha):
-    for ch in alpha[:5]:
+    letters = ''
+    for ch in alpha:
         if ch[2] == "e":
-            the = ch
-            ttt = ch[0]
-            hhh = ch[1]
+            letters += ch[:2]
+    ttt = letters[0]
+    hhh = letters[1]
+    the = ttt+hhh+'e'
     print("\nStep2\n'the' is one of the most frequent trigram.")
-    print("Since we have already solve 'e' and if we take a look at the bigram table, 'the' is ","'",the,"'", sep="")
+    print("Since we have already solve 'e' and if we take a look at the trigram table, 'the' is '",the,"'")
     print("\nWe are going to replace all '",ttt,"' with 't' and '", hhh, "' with h as followed:\n")
-    mystring = mystring.replace(ttt,"t") ## Replace t
-    mystring = mystring.replace(hhh,"h") ## Replace h
+    mystring_t = mystring.replace(ttt,"t") ## Replace t
+    mystring_th = mystring_t.replace(hhh,"h") ## Replace h
     mystring_ = ""
-    for ch in mystring:
-        if ch == "t":
-            mystring_ += ch
-        elif ch == "h":
-            mystring_ += ch
-        elif ch.isalpha():
+    for ch in mystring_th:
+        if ch.isupper():
             mystring_ += "_"
         else:
             mystring_ += ch
     print(mystring_)
-    return mystring 
+    return mystring_th
 
+###################################################################################################################
+#######################                     FIND N 
+###################################################################################################################
 
+## Define step3 --> Find n and replace it
+def find_n(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[0] == "e" and ch[2] == "t":
+            letters += ch[1]
+    nnn = letters[0]
+    ent = "e"+nnn+"t"
+    print("\nStep3\n'ent' is the most frequent trigram of 'e(something)t'.")
+    print("Since we have already solved 'e' and 't' and if we take a look at the trigram table, 'ent' is '",ent,"'")
+    print("\nWe are going to replace all '",nnn,"' with 'n' as followed:\n")
+    mystring_n = mystring.replace(nnn,"n") ## Replace n
+    mystring_ = ""
+    for ch in mystring_n:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_n
 
+###################################################################################################################
+#######################                     FIND A
+###################################################################################################################
 
+## Define step4 --> Find a and replace it
+def find_a(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[0] == "t" and ch[1] == "h" and ch[3] == "t":
+            letters += ch[2]
+    aaa = letters[0]
+    that = "t"+"h"+aaa+"t"
+    print("\nStep4\n'that' is the most frequent 4-gram of 'th(something)t'.")
+    print("Since we have already solved 't' and 'h' and if we take a look at the 4-gram table, 'that' is '",that,"'")
+    print("\nWe are going to replace all '",aaa,"' with 'a' as followed:\n")
+    mystring_a = mystring.replace(aaa,"a") ## Replace a
+    mystring_ = ""
+    for ch in mystring_a:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_a
+
+###################################################################################################################
+#######################                     FIND D
+###################################################################################################################
+
+## Define step5 --> Find d and replace it
+def find_d(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[:2] == "an":
+            letters += ch[2]
+    ddd = letters[0]
+    and_ = "a"+"n"+ddd
+    print("\nStep5\n'and' is the most frequent trigram of 'an(something)'.")
+    print("Since we have already solved 'a' and 'n' and if we take a look at the 3-gram table, 'and' is '",and_,"'")
+    print("\nWe are going to replace all '",ddd,"' with 'd' as followed:\n")
+    mystring_d = mystring.replace(ddd,"d") ## Replace d
+    mystring_ = ""
+    for ch in mystring_d:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_d
+
+###################################################################################################################
+#######################                     FIND R 
+###################################################################################################################
+
+## Define step6 --> Find r and replace it
+def find_r(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[1] == "e" and ch[0].isupper():
+            letters += ch[0]
+    rrr = letters[0]
+    re = rrr+"e"
+    print("\nStep6\n're' is the most frequent bigram of '(something)e' excluding the bigrams that we have already decrypted.")
+    print("Since we have already solved 'he' and 'te', hence if we take a look at the bigrams table, 're' is '",re,"'")
+    print("\nWe are going to replace all '",rrr,"' with 'r' as followed:\n")
+    mystring_r = mystring.replace(rrr,"r") ## Replace r
+    mystring_ = ""
+    for ch in mystring_r:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_r
+
+###################################################################################################################
+#######################                     FIND O
+###################################################################################################################
+
+## Define step7 --> Find o and replace it
+def find_o(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[1] == "r" and ch[0].isupper():
+            letters += ch[0]
+    ooo = letters[0]
+    or_ = ooo+"r"
+    print("\nStep7\n'or' is the most frequent bigram of '(something)r' excluding the bigrams that we have already decrypted.")
+    print("Since we have already solved 'er' and 'ar', hence if we take a look at the bigrams table, 'or' is '",or_,"'")
+    print("\nWe are going to replace all '",ooo,"' with 'o' as followed:\n")
+    mystring_o = mystring.replace(ooo,"o") ## Replace o
+    mystring_ = ""
+    for ch in mystring_o:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_o
+
+###################################################################################################################
+#######################                     FIND I
+###################################################################################################################
+
+## Define step8 --> Find i and replace it
+def find_i(mystring, alpha):
+    letters = ''
+    for ch in alpha:
+        if ch[1] == "n" and ch[0].isupper():
+            letters += ch[0]
+    iii = letters[0]
+    in_ = iii+"n"
+    print("\nStep8\n'or' is the most frequent bigram of '(something)n' excluding the bigrams that we have already decrypted.")
+    print("Since we have already solved 'an' , 'on' and 'en', hence if we take a look at the bigrams table, 'in' is '",in_,"'")
+    print("\nWe are going to replace all '",iii,"' with 'i' as followed:\n")
+    mystring_i = mystring.replace(iii,"i") ## Replace i
+    mystring_ = ""
+    for ch in mystring_i:
+        if ch.isupper():
+            mystring_ += "_"
+        else:
+            mystring_ += ch
+    print(mystring_)
+    return mystring_i
 
 
 
